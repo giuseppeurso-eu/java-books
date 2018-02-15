31078157f6705d90b26e6fd801831fd3390bfadb
giuseppeurso-eu <urso.giu@gmail.com>
2018-02-15 13:21:04 +0100

    First commit of effective-java project

---
+++ effective-java/src/main/java/eu/giuseppeurso/effectivejava/chapter2/creatingAndDestroyingObjects/item1.java
@@ -0,0 +1,20 @@
+package eu.giuseppeurso.effectivejava.chapter2.creatingAndDestroyingObjects;
+
+/**
+ * Consider static factory methods instead of constructors.
+ *
+ * @author Giuseppe Urso - www.giuseppeurso.eu
+ *
+ */
+public class item1 {
+
+	public item1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ effective-java/src/main/java/eu/giuseppeurso/effectivejava/chapter2/creatingAndDestroyingObjects/item2.java
@@ -0,0 +1,14 @@
+package eu.giuseppeurso.effectivejava.chapter2.creatingAndDestroyingObjects;
+
+public class item2 {
+
+	public item2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
